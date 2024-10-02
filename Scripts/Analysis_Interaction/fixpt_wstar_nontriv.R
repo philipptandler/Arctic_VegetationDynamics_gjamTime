e9413f8e74b2ad804977c98ffd678f919541b441
Philipp Tandler <philipp.tandler@protonmail.ch>
2024-10-02 18:20:18 +0200

    added printstatements

--- Scripts/Analysis_Interaction/fixpt_wstar_nontriv.R
+++ Scripts/Analysis_Interaction/fixpt_wstar_nontriv.R
@@ -9,28 +9,31 @@ source("Scripts/Analysis_Interaction/analysisHfunctions.R")
 alpha <- readRDS(file.path(path_analysis_scripts, ".alphaMu.rds"))
 rho <- readRDS(file.path(path_analysis_scripts, ".rhoMu.rds"))
 
-## wstar 1990 ####
-x_1990 <- rast(file.path(path_analysis_data_rast,"x_1990.tif"))
-wstar_1990 <- matrixProd(-inv(alpha) %*% rho, x_1990) 
-names(wstar_1990) <- rownames(rho)
-writeRaster(wstar_1990,
-            file.path(path_analysis_data_rast, "wstar_1990_nontriv.tif"),
-            datatype = "INT2S", overwrite = TRUE)
-wstar_1990 <- rast(file.path(path_analysis_data_rast, "wstar_1990_nontriv.tif"))
-
-
-## negative values mask 
-mask_wstar_1990_nonneg <- (wstar_1990[[1]] >= 0 &
-                             wstar_1990[[2]] >= 0 &
-                             wstar_1990[[3]] >= 0 &
-                             wstar_1990[[4]] >= 0)
-
-writeRaster(mask_wstar_1990_nonneg,
-            file.path(path_analysis_data_rast, "mask_wstar_1990_nonneg.tif"),
-            overwrite =TRUE)
+# ## wstar 1990 ####
+# cat("calculate wstar_1990\n")
+# x_1990 <- rast(file.path(path_analysis_data_rast,"x_1990.tif"))
+# wstar_1990 <- matrixProd(-inv(alpha) %*% rho, x_1990) 
+# names(wstar_1990) <- rownames(rho)
+# writeRaster(wstar_1990,
+#             file.path(path_analysis_data_rast, "wstar_1990_nontriv.tif"),
+#             datatype = "INT2S", overwrite = TRUE)
+# wstar_1990 <- rast(file.path(path_analysis_data_rast, "wstar_1990_nontriv.tif"))
+# 
+# 
+# ## negative values mask 
+# cat("calculate wstar_1990_nonneg\n")
+# mask_wstar_1990_nonneg <- (wstar_1990[[1]] >= 0 &
+#                              wstar_1990[[2]] >= 0 &
+#                              wstar_1990[[3]] >= 0 &
+#                              wstar_1990[[4]] >= 0)
+# 
+# writeRaster(mask_wstar_1990_nonneg,
+#             file.path(path_analysis_data_rast, "mask_wstar_1990_nonneg.tif"),
+#             overwrite =TRUE)
 
 
 ## wstar 2020 ####
+cat("calculate wstar_2020\n")
 x_2020 <- rast(file.path(path_analysis_data_rast,"x_2020.tif"))
 wstar_2020 <- matrixProd(-inv(alpha) %*% rho, x_2020)
 names(wstar_2020) <- rownames(rho)
@@ -41,6 +44,7 @@ wstar_2020 <- rast(file.path(path_analysis_data_rast, "wstar_2020_nontriv.tif"))
 
 
 ## negative values mask
+cat("calculate wstar_2020_nonneg\n")
 mask_wstar_2020_nonneg <- (wstar_2020[[1]] >= 0 &
                              wstar_2020[[2]] >= 0 &
                              wstar_2020[[3]] >= 0 &
@@ -51,6 +55,7 @@ writeRaster(mask_wstar_2020_nonneg,
             overwrite =TRUE)
 
 ## wstar 2100 ####
+cat("calculate wstar_2100\n")
 x_2100 <- rast(file.path(path_analysis_data_rast,"x_2100.tif"))
 wstar_2100 <- matrixProd(-inv(alpha) %*% rho, x_2100)
 names(wstar_2100) <- rownames(rho)
@@ -60,6 +65,7 @@ writeRaster(wstar_2100,
 wstar_2100 <- rast(file.path(path_analysis_data_rast, "wstar_2100_nontriv.tif"))
 
 ## negative values mask
+cat("calculate wstar_2100_nonneg\n")
 mask_wstar_2100_nonneg <- (wstar_2100[[1]] >= 0 &
                               wstar_2100[[2]] >= 0 &
                               wstar_2100[[3]] >= 0 &
