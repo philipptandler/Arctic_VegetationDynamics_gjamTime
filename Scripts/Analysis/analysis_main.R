47b4883cd0438127733b1b0a9f8d84c5bdb32ffe
Philipp Tandler <philipp.tandler@protonmail.ch>
2024-10-10 18:36:00 -0700

    prepared for linear models2

--- Scripts/Analysis/analysis_main.R
+++ Scripts/Analysis/analysis_main.R
@@ -26,78 +26,80 @@ ndvi_sig_nf <- rast(file.path(path_ndvi, paste0(name_ndvi_sig, "_nofire.tif")))
 
 response_list <- list(
   "ndvi_abs_trend"=ndvi_abs_trend,
-  "ndvi_abs_sig"=ndvi_abs_sig,
   "ndvi_trend"=ndvi_trend,
-  "ndvi_sig"=ndvi_sig,
   "ndvi_abs_trend_nf"=ndvi_abs_trend_nf,
-  "ndvi_abs_sig_nf"=ndvi_abs_sig_nf,
   "ndvi_trend_nf"=ndvi_trend_nf,
+  "ndvi_abs_sig"=ndvi_abs_sig,
+  "ndvi_sig"=ndvi_sig,
+  "ndvi_abs_sig_nf"=ndvi_abs_sig_nf,
   "ndvi_sig_nf"=ndvi_trend_nf
 )
 
 ## load predictors
-lamda_shcf_1990 <- rast(file.path(path_analysis_data_rast,"lamda_shcf_harmonic_1990.tif"))
-lamda_shcf_2020 <- rast(file.path(path_analysis_data_rast,"lamda_shcf_harmonic_2020.tif"))
-lamda_shcf <- mean()
+#lamda harmonic
+lamda_shcf_1990_harm <- rast(file.path(path_analysis_data_rast,"lamda_shcf_harmonic_1990.tif"))
+lamda_shcf_2020_harm <- rast(file.path(path_analysis_data_rast,"lamda_shcf_harmonic_2020.tif"))
+lamda_shcf_harm <- mean(lamda_shcf_1990, lamda_shcf_2020)
+lamda_shcf_harm <- WriteAndLoad(lamda_shcf, "lamda_shcf_harmonic_mean_1990-2020",
+                           path = path_analysis_data_rast,
+                           datatype = "FLT4S")
+#lamda dominant
+lamda_shcf_1990_dom <- rast(file.path(path_analysis_data_rast,"lamda_shcf_dominant_1990.tif"))
+lamda_shcf_2020_dom <- rast(file.path(path_analysis_data_rast,"lamda_shcf_dominant_2020.tif"))
+lamda_shcf_dom <- mean(lamda_shcf_1990, lamda_shcf_2020)
+lamda_shcf_dom <- WriteAndLoad(lamda_shcf, "lamda_shcf_dominant_mean_1990-2020",
+                           path = path_analysis_data_rast,
+                           datatype = "FLT4S")
+#lamda harmonic = lamda dominant (Jacobian for only shrub has only 1 ev)
 lamda_sh <- rast(file.path(path_analysis_data_rast,"lamda_sh_mean_1990-2020.tif"))
 
 predictor_list <- list(
   "lamda_sh"=lamda_sh,
-  "tau_sh"=tau_sh,
-  "lamda_shcf"=
+  "lamda_shcf_harmonic"=lamda_shcf_harm,
+  "lamda_shcf_dominant"=lamda_shcf_dom
 )
 
-sink("output.txt")
+sink(file.path(path_analysis_data_rast,"outputLM.txt"))
+subsample <- FALSE
+subSize <- 10000
 ## linear model
 for(response in names(response_list)){
   for(predictor in names(predictor_list)){
     r <- c(response_list[[response]],predictor_list[[predictor]])
     cat("=====================================================================\n")
-    if(sample){
-      for(i in 1:n_samples){
-        r_subs <- spatSample(r, 10000)
-        cat("Linear Model",i,":", response, "~", predictor,":\n")
-        y <- values(r_subs[[1]])
-        x <- values(r_subs[[2]])
-        this_lm <- lm(y ~ x)
-        print(summary(this_lm))
-      }
-    }else{
-      cat("Linear Model:", response, "~", predictor,":\n")
-      y <- values(r[[1]])
-      x <- values(r[[2]])
-      this_lm <- lm(y ~ x)
-      print(summary(this_lm))
-      cat("\n\n\n")
-    }
-    cat("\n\n\n\n\n")
+    if(subsample){r <- spatSample(r, subSize)}
+    y <- r[,1]
+    x <- r[,2]
+    lm_this <- lm(y~x)
+    cat("Linear Model:", response, "~", predictor,":\n")
+    print(summary(lm_this))
+    plot(y~x, cex = 0.05, pch = 16, ylab = response, xlab = predictor)
+    abline(h=0)
+    abline(a = lm_this$coefficients[1], b = lm_this$coefficients[2])
+    cat("\n\n\n")
   }
 }
 sink()
 
-## for Rstudio
-# r[[1]] is treated as y, r[[2]] is treated as x
-printlm <- function(r, sample=TRUE, size = 1, n_samples=1){
-  if(sample){
-    for(i in 1:n_samples){
-      r_subs <- spatSample(r, size)
-      y <- r_subs[,1]
-      x <- r_subs[,2]
-      lm_this <- lm(y~x)
-      cat("Linear Model",i,":", names(r_subs[1]), "~", names(r_subs[2]),":\n")
-      cat("\ny:", names(r_subs)[1], "\n")
-      print(summary(y))
-      cat("\nx:", names(r_subs)[2], "\n")
-      print(summary(x))
-      print(summary(lm_this))
-      plot(y~x, cex = 0.1, pch = 16)
-      abline(h=0)
-      abline(a = lm_this$coefficients[1], b = lm_this$coefficients[2])
-      cat("\n\n\n")
-    }
-  }
-}
-
-printlm(c(ndvi_abs_trend_nf, lamda_sh), size = 10000, n_samples = 1)
+## for Rstudio, comment out
+# # r[[1]] is treated as y, r[[2]] is treated as x
+# printlm <- function(r, sample=TRUE, size = 1, n_samples=1){
+#   if(TRUE){
+#     for(i in 1:n_samples){
+#       r_subs <- spatSample(r, size)
+#       y <- r_subs[,1]
+#       x <- r_subs[,2]
+#       lm_this <- lm(y~x)
+#       cat("Linear Model",i,":", names(r_subs[1]), "~", names(r_subs[2]),":\n")
+#       print(summary(lm_this))
+#       plot(y~x, cex = 0.05, pch = 16)
+#       abline(h=0)
+#       abline(a = lm_this$coefficients[1], b = lm_this$coefficients[2])
+#       cat("\n\n\n")
+#     }
+#   }
+# }
+# 
+# printlm(c(ndvi_abs_trend_nf, lamda_sh), size = 1000, n_samples = 1)
 
 
