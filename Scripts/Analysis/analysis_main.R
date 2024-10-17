ef247c926d0d15ab2c226ffb0a2935154bb9d226
Philipp Tandler <philipp.tandler@protonmail.ch>
2024-10-17 12:54:12 -0700

    added wrate (Stability resistance)

--- Scripts/Analysis/analysis_main.R
+++ Scripts/Analysis/analysis_main.R
@@ -24,38 +24,35 @@ ndvi_sig_nf <- rast(file.path(path_ndvi, paste0(name_ndvi_sig, "_nofire.tif")))
 
 
 response_list <- list(
-  "ndvi_abs_trend"=ndvi_abs_trend,
   "ndvi_trend"=ndvi_trend,
-  "ndvi_abs_trend_nf"=ndvi_abs_trend_nf,
   "ndvi_trend_nf"=ndvi_trend_nf,
-  "ndvi_abs_sig"=ndvi_abs_sig,
   "ndvi_sig"=ndvi_sig,
-  "ndvi_abs_sig_nf"=ndvi_abs_sig_nf,
   "ndvi_sig_nf"=ndvi_sig_nf
 )
 
 ## load predictors
-#lamda shurb
 lamda_sh <- rast(file.path(path_analysis_data_rast,"lamda_sh_mean_1990-2020.tif"))
-#lamda conifer
 lamda_cf <- rast(file.path(path_analysis_data_rast,"lamda_cf_mean_1990-2020.tif"))
-#lamda_shurb_conifer
 lamda_shcf_harm <- rast(file.path(path_analysis_data_rast,
                                   "lamda_shcf_harmonic_mean_1990-2020.tif"))
-lamda_shcf_dom <- rast(file.path(path_analysis_data_rast,
-                                 "lamda_shcf_dominant_mean_1990-2020.tif"))
+
+wrate_sh <- rast(file.path(path_analysis_data_rast,"wrate_sh.tif"))
+wrate_cf <- rast(file.path(path_analysis_data_rast,"wrate_cf.tif"))
+wrate_shcf <- rast(file.path(path_analysis_data_rast,"wrate_shcf.tif"))
 
 
 predictor_list <- list(
   "lamda_sh"=lamda_sh,
   "lamda_cf"=lamda_cf,
   "lamda_shcf_harmonic"=lamda_shcf_harm,
-  "lamda_shcf_dominant"=lamda_shcf_dom
+  "wrate_sh" = wrate_sh,
+  "wrate_cf" = wrate_cf,
+  "wrate_shcf" = wrate_shcf
 )
 
 
 ## run Linear Models for all combinations ####
-sink(file.path(path_analysis_data_rast,"LM_coefficients.txt"))
+sink(file.path(path_analysis_data_rast,"LM_overview.txt"))
 subsample <- FALSE
 plot <- FALSE
 subSize <- 1000
@@ -83,16 +80,14 @@ for(response in names(response_list)){
   cat("\n\n\n\n")
 }
 
-saveRDS(lm_list, file = file.path(path_analysis_data_rast, "lm_list.rds"))
+saveRDS(lm_list, file = file.path(path_analysis_data_rast, "lm_sum.rds"))
 
 cat("=====================================================================\n")
 lm_matrix <- lm_matrix_summary(lm_list, coef="slope", measure = "estimate")
-print(lm_matrix[,1:4])
-print(lm_matrix[,5:8])
+print(lm_matrix)
 cat("\n\n")
 lm_matrix <- lm_matrix_summary(lm_list, coef="slope", measure = "p-value")
-print(lm_matrix[,1:4])
-print(lm_matrix[,5:8])
+print(lm_matrix)
 sink()
 
 
@@ -107,7 +102,7 @@ sink()
 #       lm_this <- lm(y~x)
 #       cat("Linear Model",i,":", names(r_subs[1]), "~", names(r_subs[2]),":\n")
 #       print(summary(lm_this))
-#       plot(y~x, cex = 0.05, pch = 16, ylab=names(r)[1], xlab=names(r)[2], xlim = c(-0.25,0))
+#       plot(y~x, cex = 0.05, pch = 16, ylab=names(r)[1], xlab=names(r)[2])
 #       abline(h=0)
 #       abline(a = lm_this$coefficients[1], b = lm_this$coefficients[2])
 #       cat("\n\n\n")
@@ -115,7 +110,4 @@ sink()
 #   }
 # }
 # 
-# printPlotlm(c(ndvi_trend, lamda_shcf_harm), size = 200000, n_samples = 1)
-# printPlotlm(c(ndvi_sig, lamda_shcf_harm), size = 200000, n_samples = 1)
-
-
+# printPlotlm(c(ndvi_sig,predictor_list[["wrate_sh"]]), size = 200000, n_samples = 1)
