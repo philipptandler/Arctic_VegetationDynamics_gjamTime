1b91442ca752c0567aab373e214074c936317f8b
Philipp Tandler <philipp.tandler@protonmail.ch>
2025-03-14 00:14:54 +0100

    adjusted echo

--- shell/run_gjamTime_chain_shell.sh
+++ shell/run_gjamTime_chain_shell.sh
@@ -3,8 +3,8 @@
 # Define setup scripts
 SETUP_SCRIPTS=(
 	"scripts/project/1_gjamTime/call_probe1_base.R"
-       	"scripts/project/1_gjamTime/call_probe1_highorder1.R"
-       	"scripts/project/1_gjamTime/call_probe1_highorder2.R"
+#       	"scripts/project/1_gjamTime/call_probe1_highorder1.R"
+#       	"scripts/project/1_gjamTime/call_probe1_highorder2.R"
 	)
 
 for scrpt in "${SETUP_SCRIPTS[@]}"; do
@@ -14,5 +14,5 @@ for scrpt in "${SETUP_SCRIPTS[@]}"; do
 
     # Submit summarize_gjamTime_shell.sh with dependency on run_gjamTime_shell.sh
     jobid2=$(sbatch --dependency=afterok:$jobid1 --export=ALL,scrpt="$scrpt" shell/summarize_gjamTime_shell.sh "$scrpt" | awk '{print $4}')
-    echo "Submitted summarize_gjamTime_shell.sbash shell/run_gjamTime_chain_shell.shh with Job ID $jobid2 and setup script $scrpt"
+    echo "Submitted summarize_gjamTime_shell.sh with Job ID $jobid2 and setup script $scrpt"
 done
