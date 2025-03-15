82945d05760d168d9661eca82cce9854946ad85e
Philipp Tandler <philipp.tandler@protonmail.ch>
2025-03-15 16:34:25 -0700

    ..

--- scripts/masterscripts/run_gjamTime_chain.R
+++ scripts/masterscripts/run_gjamTime_chain.R
@@ -12,10 +12,11 @@ setwd(here::here())
 call_this_script <- "scripts/project/1_gjamTime/call_gjamTime_test1.R"
 
 # set replicates for multiple simulations here
-n_replicates <- 2
+n_replicates <- 10
 
 # runs gjamTime independently
 for(run in 1:n_replicates){
+  cat("run_gjamTime_batch run", run, "out of", n_replicates, "\n")
   task_id <- run
   call_scrpt <- call_this_script
   source("scripts/masterscripts/run_gjamTime_batch.R")
