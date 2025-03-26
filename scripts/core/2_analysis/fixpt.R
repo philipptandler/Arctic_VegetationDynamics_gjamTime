aba3c0bb1ebf4e9b5df1a2f65c0140b72ce806b9
Philipp Tandler <philipp.tandler@protonmail.ch>
2025-03-26 15:26:15 -0700

    new script to directly calculate fixed point

---
+++ scripts/core/2_analysis/fixpt.R
@@ -0,0 +1,9 @@
+## sourcing general scripts ####
+source("scripts/core/2_analysis/.analysis_Hfunctions.R")
+
+fixpt <- function(rho, alpha, x){
+  
+  wstar <- .fixpt(beta = NULL, rho = rho, alpha = alpha, x)
+  
+  wstar
+}
