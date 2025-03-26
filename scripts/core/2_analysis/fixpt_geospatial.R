41b828fa026d452d993e2a80cf173e492c8eeb39
Philipp Tandler <philipp.tandler@protonmail.ch>
2025-03-26 11:34:20 -0700

    new mask for calling .fixpt()

--- scripts/core/2_analysis/fixpt_geospatial.R
+++ scripts/core/2_analysis/fixpt_geospatial.R
@@ -1,8 +1,9 @@
 ## sourcing general scripts ####
 source("scripts/core/2_analysis/.analysis_Hfunctions.R")
-fixpt_geospatial <- function(arg, out_folder = NULL, output_mask = NULL){
+
+fixpt_geospatial <- function(argument, ...){
   
-  wstar_list <- .fixpt_geospatial(arg, out_folder, output_mask)
+  wstar_list <- .fixpt_geospatial(arg, ...)
   
   wstar_list
 }
