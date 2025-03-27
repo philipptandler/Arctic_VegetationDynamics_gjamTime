3f7e2b63b50772d4c07b4b0219b7fdbe618b3b05
Philipp Tandler <philipp.tandler@protonmail.ch>
2025-03-27 10:44:36 -0700

    fixed 'arg' -> 'argument'

--- scripts/core/2_analysis/fixpt_geospatial.R
+++ scripts/core/2_analysis/fixpt_geospatial.R
@@ -3,7 +3,7 @@ source("scripts/core/2_analysis/.analysis_Hfunctions.R")
 
 fixpt_geospatial <- function(argument, ...){
   
-  wstar_list <- .fixpt_geospatial(arg, ...)
+  wstar_list <- .fixpt_geospatial(argument, ...)
   
   wstar_list
 }
