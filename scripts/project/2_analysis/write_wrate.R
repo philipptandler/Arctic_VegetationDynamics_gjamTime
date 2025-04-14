5c42ce047cd936a01aac62aa8594f91cc9ea5a2c
Philipp Tandler <philipp.tandler@protonmail.ch>
2025-04-14 13:41:03 -0700

    find linear model of fixed points

--- scripts/project/2_analysis/write_wrate.R
+++ scripts/project/2_analysis/write_wrate.R
@@ -5,12 +5,12 @@ setwd(here::here())
 source("config/config_local.R")
 source("scripts/core/2_analysis/wrate_geospatial.R")
 
-folder <- "test_higherorder1_alphaNeg1"
-type = "w_obs" # match file name
+folder <- "probe1_base"
+type = "w_star" # match file name
 
 # assert correct order
 files <- list.files(file.path(path_analysis, folder), pattern = type,
                     full.names = TRUE)
 
 ## call fixpt_geospatial()
-wobs <- wrate_geospatial(files, rate = TRUE, linear_model = TRUE, datatype = "INT2S")
+wobs <- wrate_geospatial(files, rate = FALSE, linear_model = TRUE, datatype = "INT2S")
