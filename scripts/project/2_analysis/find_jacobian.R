31f335cfdc5778f236ff8a442ae4b146b7b09258
Philipp Tandler <philipp.tandler@protonmail.ch>
2025-04-22 09:10:53 -0700

    change name comment

--- scripts/project/2_analysis/find_jacobian.R
+++ scripts/project/2_analysis/find_jacobian.R
@@ -8,6 +8,6 @@ source("scripts/core/2_analysis/jacobian_geospatial.R")
 arg <- "probe1_base"
 # arg <- "test_higherorder1_alphaNeg1"
 
-## call fixpt_geospatial()
+## call jacobian_geospatial()
 jacobian <- jacobian_geospatial(argument = arg, n_chunks = 100, regular = TRUE,
                                 inverse = FALSE)
