1d28dc6c4843d4a339f6596c57e42ead3c1ad935
Philipp Tandler <philipp.tandler@protonmail.ch>
2025-04-29 02:28:35 +0200

    adjust output mas

--- shell/chain_jobs_shell.sh
+++ shell/chain_jobs_shell.sh
@@ -7,11 +7,11 @@ SETUP_ARGUMENTS=(
 
 for arg in "${SETUP_ARGUMENTS[@]}"; do
     # Submit run_gjamTime_shell.sh
-    jobid1=$(sbatch shell/run_gjamTime_shell.sh "$arg" | awk '{print $4}')
-    echo "Submitted run_gjamTime_shell.sh with Job ID $jobid1 and argument $arg"
+    # jobid1=$(sbatch shell/run_gjamTime_shell.sh "$arg" | awk '{print $4}')
+    # echo "Submitted run_gjamTime_shell.sh with Job ID $jobid1 and argument $arg"
 
     # Submit summarize_gjamTime_shell.sh with dependency on run_gjamTime_shell.sh
-    jobid2=$(sbatch --dependency=afterok:$jobid1 shell/summarize_gjamTime_shell.sh "$arg" | awk '{print $4}')
+    jobid2=$(sbatch --dependency=afterok:30354303 shell/summarize_gjamTime_shell.sh "$arg" | awk '{print $4}')
     echo "Submitted summarize_gjamTime_shell.sh with Job ID $jobid2 and argument $arg"
     
     # Submit find_fixpt_shell.sh with dependency on summarize_gjamTime_shell.sh
