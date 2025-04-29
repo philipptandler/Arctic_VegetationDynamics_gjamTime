96deee6264c0d445e283d53cc03b196b3f67dcc8
Philipp Tandler <philipp.tandler@protonmail.ch>
2025-04-28 17:29:18 -0700

    adjust output mask

--- scripts/masterscripts/find_fixpt_batch.R
+++ scripts/masterscripts/find_fixpt_batch.R
@@ -20,4 +20,5 @@ if(!exists("arg")){
 
 ## call fixpt_geospatial()
 # first argument is arg, second times_out
-wstar <- fixpt_geospatial(argument = arg, times_out = times_out, data_type = "INT2S")
+wstar <- fixpt_geospatial(argument = arg, times_out = times_out, data_type = "INT2S",
+                          output_mask = file.path(path_masks, "wildfire_mask_1978-2014.tif"))
--- scripts/project/2_analysis/find_jacobian.R
+++ scripts/project/2_analysis/find_jacobian.R
@@ -5,9 +5,10 @@ setwd(here::here())
 source("config/config_local.R")
 source("scripts/core/2_analysis/jacobian_geospatial.R")
 
-arg <- "probe1_base"
+arg <- "probe1_base_nf"
 # arg <- "test_higherorder1_alphaNeg1"
 
 ## call jacobian_geospatial()
 jacobian <- jacobian_geospatial(argument = arg, n_chunks = 100, regular = TRUE,
-                                inverse = FALSE)
+                                inverse = FALSE,
+                                output_mask = file.path(path_masks, "wildfire_mask_1978-2014.tif"))
